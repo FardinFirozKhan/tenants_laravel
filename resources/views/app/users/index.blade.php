fd8d7ffe7cdb660d4970ec58890395c726309439
FardinFirozKhan <fardinfirozkhan007@gmail.com>
2024-05-11 13:27:10 +0600

    first commit

---
+++ resources/views/app/users/index.blade.php
@@ -0,0 +1,61 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Tenants') }}
+            <x-btn-link class="ml-4 float-right" href="{{ route('tenants.create') }}">Add Tenants</x-btn-link>
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+
+                <div class="py-12">
+
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        Name
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Email
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Domain
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Action
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($tenants as $tenant)
+                                    <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {{ $tenant->name }}
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            {{ $tenant->email }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            @foreach ($tenant->domains as $domain)
+                                                {{ $domain->domain }} {{ $loop->last ? ''  :',' }}
+                                            @endforeach
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+
+                            </tbody>
+                        </table>
+                    </div>
+
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
